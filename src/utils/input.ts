eb47207728bda1eee1af8c2b903a8857e954e8e7
Pawel Bartusiak <21136755+uFloppyDisk@users.noreply.github.com>
2025-01-04 17:45:46 -0800

    fix: readInput importing wrong dir;
    Import input text file from day's dir;

--- src/utils/input.ts
+++ src/utils/input.ts
@@ -1,11 +1,18 @@
 import {readFileSync} from 'fs';
 import * as path from 'path';
+import {default as callsite} from 'callsite';
 
 export function readInput(override?: InputOverride): string {
+  const stack = callsite();
+  const requestFile = stack[1].getFileName();
+
   return (
     override?.raw ??
-    readFileSync(path.join(__dirname, override?.path ?? '/input.txt'), {
-      encoding: 'utf8',
-    })
+    readFileSync(
+      path.join(path.dirname(requestFile), override?.path ?? '/input.txt'),
+      {
+        encoding: 'utf8',
+      },
+    )
   );
 }
